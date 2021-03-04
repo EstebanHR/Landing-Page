d5483165ee907500319ae8127d02a213cfa55c82
EstebanHR <estebanhr1981@hotmail.com>
2021-03-04 02:46:56 +0000

    Final version

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,12 +4,20 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
+import { Cards } from "./cards";
+import { Footer } from "./footer";
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
 			<Navbar />
 			<Jumbotron />
+			<div className="d-flex justify-content-center">
+				<Cards />
+				<Cards />
+				<Cards />
+			</div>
+			<Footer />
 		</div>
 	);
 }
